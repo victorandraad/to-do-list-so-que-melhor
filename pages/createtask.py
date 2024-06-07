00c0ab0b0f7a1d1627ae42cca04639c330230c13
vouto <68758638+andradavic@users.noreply.github.com>
2024-06-07 00:18:51 -0300

    storing values in variables

--- pages/createtask.py
+++ pages/createtask.py
@@ -1,55 +1,64 @@
 from flet import *
+from database.db import *
 
 cor = "white"
 
 def deck_name_field():
+    global deck_name
+    deck_name = TextField(label="Nome do Deck", width=272, height=43, color=cor, border_color='blue')
     return Container(
         content=Row(
             alignment=MainAxisAlignment.CENTER,
             controls=[
-                TextField(label="Nome do Deck", width=272, height=43, color=cor, border_color='blue')
+                deck_name
             ]
         )
     )
  
 def task_time_field():
+    global task_time
+    task_time = CupertinoTextField(width=50, placeholder_text='25', text_align='center', value=25)
     return Container(
         padding=10,
         content=Row(
             alignment=MainAxisAlignment.CENTER,
             controls=[
                 Text(value="Tempo padrão de tasks em minutos", font_family="Roboto", color =cor),
-                CupertinoTextField(width=50, placeholder_text='25', text_align='center')
+                task_time
             ]
         )
     )
 
 def break_time_field():
+    global break_time
+    break_time = CupertinoTextField(width=50, placeholder_text='5', text_align='center', value=5)
     return Container(
         padding=10,
         content=Row(
             alignment=MainAxisAlignment.CENTER,
             controls=[
                 Text(value="Tempo padrão de tasks em minutos", font_family="Roboto", color =cor),
-                CupertinoTextField(width=50, placeholder_text='5', text_align='center')
+                break_time
             ]
         )
     )
 
 def repeat_time_field():
+    global cicles
+    cicles = CupertinoTextField(width=50, placeholder_text='3', text_align='center', value=3)
     return Container(
         padding=10,
         content=Row(
             alignment=MainAxisAlignment.CENTER,
             controls=[
                 Text(value="Quantidade de repetições padrão", font_family="Roboto", color =cor),
-                CupertinoTextField(width=50, placeholder_text='3', text_align='center')
+                cicles
             ]
         )
     )
 
 def ring(page):
-
+    global selected_files
     def pick_files_result(e: FilePickerResultEvent):
         selected_files.value = (
             ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
@@ -57,7 +66,7 @@ def ring(page):
         selected_files.update()
 
     pick_files_dialog = FilePicker(on_result=pick_files_result)
-    selected_files = Text(value="Som de alarme padrão", font_family="Roboto", color =cor, overflow='ELLIPSIS')
+    selected_files = Text(value='alert-sound-loop-189741.mp3', font_family="Roboto", color =cor, overflow='ELLIPSIS')
 
     page.overlay.append(pick_files_dialog)
 
@@ -78,6 +87,11 @@ def ring(page):
         )
     )
 
+def create_deck(page):
+    Database(deck_name.value)
+    Decks().configs(deck_name.value, task_time.value, break_time.value, cicles.value, selected_files.value)
+    page.go("/")
+
 def footer(page):
     return Container(
         padding=20,
@@ -86,7 +100,7 @@ def footer(page):
             vertical_alignment=VerticalAlignment.END,
             controls=[
                 ElevatedButton("Cancelar", on_click= lambda _: page.go("/")),
-                ElevatedButton("Criar", on_click= lambda _: page.go("/"))
+                ElevatedButton("Criar", on_click= lambda _: create_deck(page))
             ]
         )
     )
