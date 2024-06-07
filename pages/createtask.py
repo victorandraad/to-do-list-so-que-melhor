fff7559d27357591a24946e5d2aacfadf2d47691
vouto <68758638+andradavic@users.noreply.github.com>
2024-06-07 13:39:06 -0300

    copy selected audio to assets

--- pages/createtask.py
+++ pages/createtask.py
@@ -1,5 +1,7 @@
 from flet import *
 from database.db import *
+import os
+import shutil
 
 cor = "white"
 
@@ -57,16 +59,22 @@ def repeat_time_field():
         )
     )
 
+from flet import FilePicker, FilePickerResultEvent, Text, ElevatedButton, Container, Row, MainAxisAlignment, icons
+
 def ring(page):
     global selected_files
     def pick_files_result(e: FilePickerResultEvent):
+        global path
         selected_files.value = (
             ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
         )
         selected_files.update()
+        path = (
+            ", ".join(map(lambda f: f.path, e.files)) if e.files else ""
+        )
 
     pick_files_dialog = FilePicker(on_result=pick_files_result)
-    selected_files = Text(value='alert-sound-loop-189741.mp3', font_family="Roboto", color =cor, overflow='ELLIPSIS')
+    selected_files = Text(value='alert-sound-loop-189741.mp3', font_family="Roboto", color='white', overflow='ELLIPSIS')
 
     page.overlay.append(pick_files_dialog)
 
@@ -79,16 +87,20 @@ def ring(page):
                 ElevatedButton(
                     "Selecionar Arquivo",
                     icon=icons.UPLOAD,
-                    on_click=lambda _: pick_files_dialog.pick_files(
-                        allow_multiple=False
-                    ),
+                    on_click=lambda _: pick_files_dialog.pick_files(allow_multiple=False, file_type=FilePickerFileType.AUDIO),
                 )
             ]
         )
     )
 
+
 def create_deck(page):
     Database(deck_name.value)
+    os.makedirs(r'assets\rings', exist_ok=True)
+
+    if not os.path.exists(f'assets\rings\{selected_files.value}'):
+        shutil.copy(path, f'assets/rings/{selected_files.value}')
+
     Decks().configs(deck_name.value, task_time.value, break_time.value, cicles.value, selected_files.value)
     page.go("/")
 
