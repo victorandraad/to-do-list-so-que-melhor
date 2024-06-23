bde5005eea540c50795f40c9d295d309f2346d50
vouto <68758638+andradavic@users.noreply.github.com>
2024-06-22 21:12:00 -0300

    default window border removed

--- main.py
+++ main.py
@@ -2,52 +2,46 @@ from flet import *
 from pages.homepage import *
 from pages.createdeck import *
 
-
 def main(page: Page):
     page.fonts = {
         "Roboto": "/fonts/Roboto-Regular.ttf",
     }
+    
     page.title = "TOMODORO"
-    page.window_width = 414
-    page.window_height = 510
-    page.window_resizable = False
+    page.window_bgcolor = colors.TRANSPARENT
+    page.bgcolor = colors.TRANSPARENT
+    page.window_title_bar_hidden = True
+    page.window_frameless = True
+    page.window_left = 400
+    page.window_top = 200
+    page.window_width = 425
+    page.window_height = 455
 
     def mainContainer(e=None):
         tasks_container = tasksContainer()
         return Container(
-        width=430,
-        height=455,
-        border_radius=10,
-        bgcolor='black' if cor == 'white' else 'white',
-        content=Column(
-            controls=[
-                menuContainer(page),
-                selectContainer(page, tasks_container),
-                inputContainer(tasks_container), 
-                tasks_container,
-                statusContainer(),
-            ]
+            border_radius= 10,
+            bgcolor= 'black',
+            content = Column(
+                [
+                    menuContainer(page),
+                    selectContainer(page, tasks_container),
+                    inputContainer(tasks_container), 
+                    tasks_container,
+                    statusContainer(),
+                ]
+            )
         )
-    )
-
     def createTaskContainer(e=None):
-        return Container(
-        width=414,
-        height=455,
-        border_radius=10,
-        bgcolor='black' if cor == 'white' else 'white',
-        content=Column(
-            controls=[
-                menuContainer(page),
-                deck_name_field(),
-                task_time_field(),
-                break_time_field(),
-                repeat_time_field(),
-                ring(page),
-                footer(page)
-            ]
-        )
-    )
+        return [
+                    menuContainer(page),
+                    deck_name_field(),
+                    task_time_field(),
+                    break_time_field(),
+                    repeat_time_field(),
+                    ring(page),
+                    footer(page)
+                ]
     
     def route_change(e: RouteChangeEvent):
         page.views.clear()
@@ -56,16 +50,16 @@ def main(page: Page):
                 "/",
                 [
                     mainContainer(),
-                ]
+                ],
+                bgcolor='transparent'
             )
         )
         if page.route == "/createtask":
             page.views.append(
                 View(
                     "/createtask",
-                    [
-                        createTaskContainer(),
-                    ]
+                    createTaskContainer(),
+                    bgcolor='black'
                 )
             )
         page.update()
